9bef6624a930193bb587711d0003311e00c6e4e3
Nicolas roggero <roggero.n@gmail.com>
2023-05-12 21:12:31 -0300

    Creacion de proyecto

---
+++ Final/Data/FinalWebContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Final.Data
+{
+    public class FinalWebContext : DbContext
+    {
+        public FinalWebContext(DbContextOptions<FinalWebContext> options) : base(options)
+        {
+
+        }
+    }
+}

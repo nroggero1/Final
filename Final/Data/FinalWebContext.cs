d7d01cc242a6ef129164a913d80aedb35413f141
Nicolas roggero <roggero.n@gmail.com>
2023-05-13 20:27:41 -0300

    Ajustes

--- Final/Data/FinalWebContext.cs
+++ Final/Data/FinalWebContext.cs
@@ -10,14 +10,17 @@ namespace Final.Data
         {
 
         }
-        public DbSet<FinalWebContext.Models.Proveedor> Proveedores { get; set; } = default!;
-        public DbSet<FinalWebContext.Models.Cliente> Clientes { get; set; } = default!;
-        public DbSet<FinalWebContext.Models.Marca> Marcas { get; set; } = default!;
-        public DbSet<FinalWebContext.Models.Categoria> Categorias { get; set; } = default!;
-        public DbSet<FinalWebContext.Models.Producto> Productos { get; set; } = default!;
-        public DbSet<FinalWebContext.Models.Usuario> Usuarios { get; set; } = default!;
-        public DbSet<FinalWebContext.Models.Compra> Compras { get; set; } = default!;
-        public DbSet<FinalWebContext.Models.Venta> Ventas { get; set; } = default!;
+
+        public DbSet<Final.Models.Categoria> Categorias { get; set; } = default!;
+        public DbSet<Final.Models.Cliente> Clientes { get; set; } = default!;
+        public DbSet<Final.Models.Compra> Compras { get; set; } = default!;
+        public DbSet<Final.Models.Localidad> Localidades { get; set; } = default!;
+        public DbSet<Final.Models.Marca> Marcas { get; set; } = default!;
+        public DbSet<Final.Models.Producto> Productos { get; set; } = default!;
+        public DbSet<Final.Models.Proveedor> Proveedores { get; set; } = default!;
+        public DbSet<Final.Models.Provincia> Provincias { get; set; } = default!;
+        public DbSet<Final.Models.Usuario> Usuarios { get; set; } = default!;
+        public DbSet<Final.Models.Venta> Ventas { get; set; } = default!;
 
     }
 

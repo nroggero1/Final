c6455502f3548b80c3f05806120628250102f7a1
Nicolas Roggero <nico_ale_roggero@hotmail.com>
2023-06-06 19:43:40 -0300

    Ajustes

--- Final/Controllers/CompraController.cs
+++ Final/Controllers/CompraController.cs
@@ -1,21 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Final.Data;
 using Final.Models;
-using Final.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Final.Controllers
 {
     public class CompraController : Controller
     {
         private readonly FinalWebContext _context;
+
         public CompraController(FinalWebContext context)
         {
             _context = context;
         }
 
-
-        public IActionResult Index()
+        public IActionResult RegistrarCompra()
         {
+            // Obtener la lista de proveedores y productos para mostrar en la vista
+            ViewBag.Proveedores = _context.Proveedor.ToList();
+            ViewBag.Productos = _context.Producto.ToList();
+
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegistrarCompra(Compra compra)
+        {
+            if (ModelState.IsValid)
+            {
+                // Agregar la compra a la lista de compras
+                // Aquí se puede usar una lista en memoria o cualquier otra estructura de datos adecuada
+                // Por ejemplo: List<Compra> listaCompras = new List<Compra>();
+                // listaCompras.Add(compra);
+
+                // Insertar los registros en la base de datos
+                _context.Compra.Add(compra);
+                _context.SaveChanges();
+
+                // Recorrer la lista de compras y guardar cada registro en la base de datos
+                //foreach (var compra in listaCompras)
+                //{
+                //    _context.Compras.Add(compra);
+                //    _context.SaveChanges();
+                //}
+
+                return RedirectToAction(nameof(Index)); // Redirigir a la página de inicio u otra página deseada
+            }
+
+            // Si la validación falla, volver a cargar la vista con los errores
+            ViewBag.Proveedores = _context.Proveedor.ToList();
+            ViewBag.Productos = _context.Producto.ToList();
+            return View(compra);
         }
     }
 }
--- Final/wwwroot/js/Producto/Producto.js
+++ Final/wwwroot/js/Producto/Producto.js
@@ -8,3 +8,9 @@ function calcularPrecioVentaSugerido() {
         document.getElementById('PrecioVentaSugerido').value = precioVentaSugerido.toFixed(2);
     }
 }
+
+function buscarProducto() {
+    var codigoBarras = document.getElementById("codigoBarras").value;
+    // Realizar una petición al servidor para buscar el producto por el código de barras
+    // y completar los campos correspondientes, como Nombre del Producto y Precio de Compra.
+}

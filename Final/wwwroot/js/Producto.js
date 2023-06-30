c163dda82d4029680f5e9bb283f67abb85f6c1a8
Nicolas Roggero <nico_ale_roggero@hotmail.com>
2023-06-30 14:43:25 -0300

    ajustes

--- Final/wwwroot/js/Producto.js
+++ Final/wwwroot/js/Producto.js
@@ -1,10 +1,10 @@
 //Funcion para calcular precio de venta sugerido
 function calcularPrecioVentaSugerido() {
-    var precioCompra = parseFloat(document.getElementById("precioCompra").value);
-    var porcentajeGanancia = parseFloat(document.getElementById("porcentajeGanancia").value);
+    var precioCompra = parseFloat(document.getElementById('precioCompra').value);
+    var porcentajeGanancia = parseFloat(document.getElementById('porcentajeGanancia').value);
 
-    var precioVentaSugerido = precioCompra + (precioCompra * (porcentajeGanancia / 100));
-
-    // Mostrar el resultado en el campo de texto de precio de venta sugerido
-    document.getElementById("precioVentaSugerido").value = precioVentaSugerido.toFixed(2);
+    if (!isNaN(precioCompra) && !isNaN(porcentajeGanancia)) {
+        var precioVentaSugerido = precioCompra * (porcentajeGanancia / 100) + precioCompra;
+        document.getElementById('PrecioVentaSugerido').value = precioVentaSugerido.toFixed(2);
+    }
 }
